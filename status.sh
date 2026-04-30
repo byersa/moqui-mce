7643d51af388c9a4bdc81f8cf9e1cac732b75242
Al Byers <byersa@gmail.com>
2026-04-29 21:09:56 -0600

    starting to connect with moqui-mcp and webmcp

--- status.sh
+++ status.sh
@@ -1,21 +1,4 @@
-	git add    .gitignore
-	git add    entity/MceEntities.xml
-	git add    mcp-host/mcp-host.js
-	git add    mcp-host/node_modules/.package-lock.json
-	git add    mcp-host/node_modules/eventsource/README.md
-	git add    mcp-host/node_modules/eventsource/dist/index.cjs
-	git add    mcp-host/node_modules/eventsource/dist/index.cjs.map
-	git add    mcp-host/node_modules/eventsource/dist/index.d.cts
-	git add    mcp-host/node_modules/eventsource/dist/index.d.ts
-	git add    mcp-host/node_modules/eventsource/dist/index.js
-	git add    mcp-host/node_modules/eventsource/dist/index.js.map
-	git add    mcp-host/node_modules/eventsource/package.json
-	git add    mcp-host/node_modules/eventsource/src/EventSource.ts
-	git add    mcp-host/node_modules/eventsource/src/types.ts
-	git add    mcp-host/package-lock.json
-	git add    mcp-host/package.json
-	git add    status.sh
-
-	git add service/MceServices.xml
-	git add service/mce/
-
+	git add   entity/MceEntities.xml
+	git add   service/mce2.rest.xml
+	git add   status.sh
+	git add service/moqui/
